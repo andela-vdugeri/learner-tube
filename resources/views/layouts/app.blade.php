b69ba3598addaff33f928b16dc497503d19380f5
andela-vdugeri <verem.dugeri@andela.co>
2015-10-23 09:36:13 +0100

    Remove comments on secure assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,11 @@
  <!-- CSS  -->
  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
  <link href="{{ asset('css/materialize.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
- {{--<link href="{{ secure_asset('css/materialize.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>--}}
+ <link href="{{ secure_asset('css/materialize.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
  <link href="{{ asset('css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
- {{--<link href="{{ secure_asset('css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>--}}
+ <link href="{{ secure_asset('css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
  <link href="{{ asset('css/font-awesome.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
- {{--<link href="{{ secure_asset('css/font-awesome.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>--}}
+ <link href="{{ secure_asset('css/font-awesome.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
 <div class="navbar-fixed">
@@ -30,10 +30,10 @@
 <!--  Scripts-->
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="{{ asset('js/materialize.js') }}"></script>
-{{--<script src="{{ secure_asset('js/materialize.js') }}"></script>--}}
+<script src="{{ secure_asset('js/materialize.js') }}"></script>
 <script src="{{ asset('js/init.js') }}"></script>
-{{--<script src="{{ secure_asset('js/init.js') }}"></script>--}}
-{{--<script src="{{ secure_asset('js/application.js') }}"></script>--}}
+<script src="{{ secure_asset('js/init.js') }}"></script>
+<script src="{{ secure_asset('js/application.js') }}"></script>
 <script src="{{ asset('js/application.js') }}"></script>
 <script>
  $(document).ready(function(){
