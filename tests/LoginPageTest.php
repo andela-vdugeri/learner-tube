07c3ed86d6d43cdb3a826651654450fddff297c4
andela-vdugeri <verem.dugeri@andela.co>
2015-10-25 13:20:38 +0100

    Write tests for Login page

---
+++ tests/LoginPageTest.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LoginPageTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+	public function testLoginPageLoadsCorrectly()
+	{
+		$this->call('GET', '/login');
+
+		$this->assertResponseOk();
+	}
+
+	public function testLoginPageHasLogo()
+	{
+		$this->visit('/login')
+			->see('Tubr');
+	}
+
+	public function testLoginPageHasNoLoginLink()
+	{
+		$this->visit('/login')
+			->dontSeeLink('login');
+	}
+
+	public function testLoginPageHasNoLogoutLink()
+	{
+		$this->visit('/login')
+			->dontSeeLink('/logout');
+	}
+
+	public function testPageHasFaceBookLogin()
+	{
+		$this->visit('/login')
+			->see('facebook');
+	}
+
+	public function testPageHasTwitterLogin()
+	{
+		$this->visit('/login')
+			->see('twitter');
+	}
+
+	public function testPageHasGithubLogin()
+	{
+		$this->visit('/login')
+			->see('github');
+	}
+
+	/**
+	 * Test if a user can log in to the system
+	 *
+	 * This test presumes that the user exists
+	 */
+	public function testLoginFormWorksCorrectly()
+	{
+		$this->visit('/login')
+			->type('danverem@gmail.com','email')
+			->type('password', 'password')
+			->press('action')
+			->seePageIs('/dashboard');
+	}
+}
